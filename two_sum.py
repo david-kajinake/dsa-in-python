b8ca2beef7efa094998f54b64cdac6b569ea07d8
david-kajinake <davidkajinakeschenezare@gmail.com>
2026-03-26 19:18:09 +0300

    Not allowing pairs that alrady exist in the list to be re-added again

--- two_sum.py
+++ two_sum.py
@@ -2,7 +2,7 @@
 """
 Find two indices , in a list ,  whose values can be added up to get the target
 """
-num_list = [ 2 , 8 , 10 , 15  , 7 , 13 , 11 , 4  ]
+num_list = [ 2 , 8 , 10 , 15  , 7 , 13 , 11 , 4  , 4 , 8 ]
 target = 15
 
 #Return all pairs that can be summed up to get the target
@@ -18,5 +18,20 @@ def two_sum_one( num_list , target ):
         else:
             continue
     return pairs_found or None
-print(two_sum_one( num_list=num_list , target=target ))
+#print(two_sum_one( num_list=num_list , target=target ))
+
+
+
+#Approach 2 : Using Hashmap -> Not allowing pairs that alrady exist in the list . eg: (7,4) , (4,7) not allowed
+def two_sum_two(num_list,target):
+    num_list.sort()
+    pairs_found = {}
+    for i in range(len(num_list)-1):
+        the_other_number = target - num_list[i]
+        if the_other_number in pairs_found:
+            continue
+        else:
+            pairs_found[num_list[i]] = the_other_number
+    return pairs_found
+print(two_sum_two(num_list = num_list , target = target ))
 
