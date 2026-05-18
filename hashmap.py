84b0a0584de2b960cecb8a6ccc1105ab046cfd7a
david-kajinake <davidkajinakeschenezare@gmail.com>
2026-05-18 14:02:02 +0300

    Added HashMap

---
+++ hashmap.py
@@ -0,0 +1,32 @@
+"""
+HashMap is a widely used data structure that stores data in key-value pairs,
+allowing for efficient insertion, removal, and lookup, typically in (O(1)) average time.
+"""
+
+"""
+Given a list of items , calculate how many times each item appears in that list
+"""
+
+#Example: a list of fruits
+data = [ 
+"banana", "lemon", "orange", "guava" , "apple", "lemon", "orange", "avocado", "lime", "passion" ,"guava"  
+       ]
+
+def check_item_count( data_list: list ) -> dict:
+    """
+    Define a hash table.
+    Loop through all items in the list. for each item, if it doesn't exist in the hash table , insert with its times of occurence. 
+    if ti exists update it exists update its times of occurence.
+    """
+    items_map = {}
+    for item in data_list:
+        if item not in items_map:
+            items_map[item] = 1
+        else:
+            items_map[item] += 1
+
+    return items_map
+
+
+#Test it 
+print( check_item_count( data_list= data ) )
